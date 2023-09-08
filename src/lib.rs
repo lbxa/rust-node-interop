c66640a53d59954233d077437c261fa08363b125
Lucas Barbosa <lucas.chu.barbosa@au.pwc.com>
2023-09-08 17:40:10 +1000

    new

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,12 @@ extern crate napi_derive;
 pub fn sum(a: i32, b: i32) -> i32 {
   a + b
 }
+
+#[napi]
+pub fn fibonacci(n: i32) -> i32 {
+  match n {
+    0 => 0,
+    1 => 1,
+    _ => fibonacci(n - 1) + fibonacci(n - 2),
+  }
+}
